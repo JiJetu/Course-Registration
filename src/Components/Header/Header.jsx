59148db102bf70235305d5e2ef5e796181d44bc8
JiJetu <jijetu2@gmail.com>
2023-09-16 11:06:55 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="container mx-auto">
+            <h1 className="text-3xl text-center font-bold pt-12 pb-8">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
